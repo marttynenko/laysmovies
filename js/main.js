10b7b24b312f7d988fad298faba385e7b32b5c19
Evgeniy Martynenko <marttynenko@gmail.com>
2020-03-03 13:19:19 +0300

    Page quiz

--- js/main.js
+++ js/main.js
@@ -1,6 +1,62 @@
-var $app = new Vue({
-  el: '#app',
-  data: {
+var Quiz = {
+  counter: 1,
+  loadQuestion: function(questionUrl,blockToAppend){
+    //questionUrl - url запроса
+    //blockToAppend - селектор блока для вставки, полученных данных
+    $.ajax({
+      type:'GET',
+      url:questionUrl,
+      success: function(data){
+       $(blockToAppend).html(data);
+      }
+    })
+  },
+  calculate: function() {
 
+  },
+  win: function(blockToAppend) {
+    $.ajax({
+      type:'GET',
+      url: 'win.html',
+      success: function(data){
+       $(blockToAppend).html(data);
+      }
+    })
+  },
+  lose: function(blockToAppend) {
+    $.ajax({
+      type:'GET',
+      url: 'lose.html',
+      success: function(data){
+        $(blockToAppend).html(data);
+      }
+    })
+  },
+  vote: function(questionUrl,blockToAppend,elCounter) {
+    if (this.counter < 5) {
+      this.loadQuestion(questionUrl,blockToAppend);
+    } else {
+      //тут организовать проверку результатов
+      //и запускать функцию win() или lose()
+      //this.lose('.screen-content.bycard');
+      this.win('.screen-content.bycard');
+    }
+    //увеличиваем счетчик ответов
+    this.counter++;
+    $(elCounter).text(this.counter);
   }
-});
+}
+document.addEventListener('DOMContentLoaded',function(){
+
+  $(document).on('change','.label-quiz input',function(){
+    setTimeout(function(){
+      Quiz.vote('question.html','.quiz','.quiz-progress-count');
+    },300);
+  });
+
+  $(document).on('click','.btn-reload',function(e){
+    e.preventDefault();
+    location.reload();
+  })
+
+});//DOMContentLoaded
